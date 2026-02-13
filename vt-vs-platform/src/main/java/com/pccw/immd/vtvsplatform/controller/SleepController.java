8c29daf4f4b1c23d7dff7f8a2003455c4f4223ca
Jore Lin <jore.wl.lin@lpstech.com>
2026-02-13 10:48:57 +0800

    day-one

--- vt-vs-platform/src/main/java/com/pccw/immd/vtvsplatform/controller/SleepController.java
+++ vt-vs-platform/src/main/java/com/pccw/immd/vtvsplatform/controller/SleepController.java
@@ -1,8 +1,11 @@
 package com.pccw.immd.vtvsplatform.controller;
 
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.locks.ReentrantLock;
 
 @RestController
 public class SleepController {
@@ -12,5 +15,33 @@ public class SleepController {
         Thread.sleep(seconds * 1000L);
         return "Slept for " + seconds + " seconds on" + Thread.currentThread();
     }
+
+
+
+    @GetMapping("block-print/{seconds}")
+    public String blockPrint(@PathVariable int seconds) throws InterruptedException {
+        synchronized  (this) {
+            Thread.sleep(seconds * 1000L);
+            return "Pinned! " + Thread.currentThread();
+        }
+    }
+
+    private  final ReentrantLock lock = new ReentrantLock();
+
+    @GetMapping("/block-fixed/{seconds}")
+    public String blockFixed(@PathVariable int seconds) throws InterruptedException {
+        lock.lock();
+        try {
+            Thread.sleep(seconds * 1000L);
+            return "Fixed  " + seconds + " seconds on" + Thread.currentThread();
+        } finally {
+            lock.unlock();
+        }
+
+
+    }
+
+
+
 
 }
